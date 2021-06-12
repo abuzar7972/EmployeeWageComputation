c771719be664a260c77a0372b4219043c5e6a605
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 13:59:39 +0530

    Calculated wages per day

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 absent=0
+workingHours=8
+wagePerhour=20
 read -p "Enter number to check employee is present or absent:: " n
 if [ $n -eq 0 ]
 	then
 	echo "Employee is absent"
 	else
-	echo "Employee is present"
+	wages=$(($workingHours*$wagePerhour))
+	echo "Per day wage:: " $wages
 fi
