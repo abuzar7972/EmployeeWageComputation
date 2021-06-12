fb0deafe1cbb256eb8f80822b70eab7a688a561e
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 14:36:48 +0530

    Getting working hours by function

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,16 +1,7 @@
 #!/bin/bash -x
-present=1
-absent=0
-workingHours=8
-wagePerhour=20
+function workingHours()
+{
+	echo "Total working hours:: $totalWorkingHours "
+}
 read -p "Enter working hours:: " totalWorkingHours
-n=1
-if [ $totalWorkingHours -eq 100 ]
-	then
-	echo "Calculating wages per month"
-	wages=$(($workingHours*$wagePerhour))
-	result=$(($wages*20))
-	echo "Wages per month:: " $result
-	else
-	echo "Working hours are remaining"
-fi
+workingHours $totalWorkingHours
