8325205a558a7d931a9be2bd35692f0b825b6838
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 14:13:32 +0530

    Calculated with case statement

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,19 +4,16 @@ absent=0
 workingHours=8
 wagePerhour=20
 read -p "Enter number to check employee is present or absent:: " n
-if [ $n -eq 0 ]
-	then
-	echo "Employee is absent"
-fi
-if [ $n -eq 1 ]
-	then
-	echo "Employee is full time present"
+case $n in
+	0)echo "Employee is absent"
+		;;
+
+	1)echo "Employee is full time present"
 	wages=$(($workingHours*$wagePerhour))
 	echo "Full time wage:: " $wages
-fi
-if [ $n -eq 2 ]
-	then
-	echo "Employee is part time present"
+		;;
+	2)echo "Employee is part time present"
 	wages=$(($workingHours*$wagePerhour))
 	echo "part time wage:: " $wages
-fi
+		;;
+esac
